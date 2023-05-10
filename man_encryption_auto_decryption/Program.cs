f1849bf9fb81b202e8998aae31711e2a2327bf3f
Magnus Strale <magnus.strale@mongodb.com>
2023-05-10 10:28:01 +0200

    Remove unnecessary schema for auto decryption

--- man_encryption_auto_decryption/Program.cs
+++ man_encryption_auto_decryption/Program.cs
@@ -1,23 +1,22 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Encryption;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 // IN VALUES HERE!
 const string PETNAME = "solid-cat";
 const string MDB_PASSWORD = "password123";
 
-const string AppUser = "app_user";
-const string CaPath = "/etc/pki/tls/certs/ca.cert";
+const string appUser = "app_user";
+const string caPath = "/etc/pki/tls/certs/ca.cert";
 
 // Note that the .NET driver requires the certificate to be in PKCS12 format. You can convert
 // the file /home/ec2-user/server.pem into PKCS12 with the command
 // openssl pkcs12 -export -out "/home/ec2-user/server.pkcs12" -in "/home/ec2-user/server.pem" -name "kmipcert"
-const string Pkcs12Path = "/home/ec2-user/server.pkcs12";
+const string pkcs12Path = "/home/ec2-user/server.pkcs12";
 
 // Obviously this should not be hardcoded
-const string connectionString = $"mongodb://{AppUser}:{MDB_PASSWORD}@csfle-mongodb-{PETNAME}.mdbtraining.net/?serverSelectionTimeoutMS=5000&tls=true&tlsCAFile={CaPath}";
+const string connectionString = $"mongodb://{appUser}:{MDB_PASSWORD}@csfle-mongodb-{PETNAME}.mdbtraining.net/?serverSelectionTimeoutMS=5000&tls=true&tlsCAFile={caPath}";
 
 // Declare our key vault namespce
 const string keyvaultDb = "__encryption";
@@ -45,108 +44,22 @@ const string encryptedCollName = "employee";
 var client = MdbClient(connectionString);
 
 // Instantiate our ClientEncryption object
-var tlsOptions = new SslSettings { ClientCertificates = new[] { new X509Certificate(Pkcs12Path) } };
+var tlsOptions = new SslSettings { ClientCertificates = new[] { new X509Certificate(pkcs12Path) } };
 var kmsTlsOptions = new Dictionary<string, SslSettings> { { provider, tlsOptions } };
 var clientEncryptionOptions = new ClientEncryptionOptions(client, keyvaultNamespace, kmsProvider, kmsTlsOptions);
 var clientEncryption = new ClientEncryption(clientEncryptionOptions);
 
-// Retrieve the DEK UUID
-var dataKeyId_1 = (await clientEncryption.GetKeyByAlternateKeyNameAsync("dataKey1"))["_id"];
-if (dataKeyId_1.IsBsonNull)
-{
-    Console.WriteLine("Failed to find DEK");
-    return;
-}
-
-var schema = new BsonDocument
-{
-    { "bsonType", "object" },
-    {
-        "encryptMetadata", new BsonDocument
-        {
-            { "keyId", dataKeyId_1 },
-            { "algorithm", "AEAD_AES_256_CBC_HMAC_SHA_512-Random" }
-        }
-    },
-    {
-        "properties", new BsonDocument {
-            {
-                "name", new BsonDocument {
-                { "bsonType", "object"} ,
-                {
-                    "properties", new BsonDocument {
-                    {
-                        "firstName", new BsonDocument {
-                        {
-                            "encrypt", new BsonDocument {
-                                { "bsonType", "string" },
-                                { "algorithm", "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic" }
-                            }
-                        } }
-                    },
-                    {
-                        "lastName", new BsonDocument {
-                        {
-                            "encrypt", new BsonDocument {
-                                { "bsonType", "string" },
-                                { "algorithm", "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic" }
-                            }
-                        } }
-                    },
-                    {
-                        "otherNames", new BsonDocument {
-                        {
-                            "encrypt", new BsonDocument { { "bsonType", "string" } }
-                        } }
-                    } }
-                } }
-            },
-            {
-                "address", new BsonDocument {
-                {
-                    "encrypt", new BsonDocument { { "bsonType", "object" } }
-                } }
-            },
-            {
-                "dob", new BsonDocument {
-                {
-                    "encrypt", new BsonDocument { { "bsonType", "date" } }
-                } }
-            },
-            {
-                "phoneNumber", new BsonDocument {
-                {
-                    "encrypt", new BsonDocument { { "bsonType", "string" } }
-                } }
-            },
-            {
-                "salary", new BsonDocument {
-                {
-                    "encrypt", new BsonDocument { { "bsonType", "double" } }
-                } }
-            },
-            {
-                "taxIdentifier", new BsonDocument {
-                {
-                    "encrypt", new BsonDocument { { "bsonType", "string" } }
-                } }
-            }
-        }
-    }
-};
-var schemaMap = new Dictionary<string, BsonDocument> { {"companyData.employee", schema } };
 var extraOptions = new Dictionary<string, object>()
 {
     { "cryptSharedLibPath", "/lib/mongo_crypt_v1.so"},
+    { "cryptSharedLibRequired", true },
+    { "mongocryptdBypassSpawn", true }
 };
 var autoEncryption = new AutoEncryptionOptions(
     kmsProviders: kmsProvider,
     keyVaultNamespace: keyvaultNamespace,
-    schemaMap: schemaMap,
-    bypassAutoEncryption: true,
     extraOptions: extraOptions,
     tlsOptions: kmsTlsOptions);
-
 var encryptedClient = MdbClient(connectionString, autoEncryption);
 
 var payload = new BsonDocument
@@ -175,6 +88,14 @@ var payload = new BsonDocument
     { "taxIdentifier", "78SDSSWN001" },
     { "role", new BsonArray { "CE" } }
 };
+
+// Retrieve the DEK UUID
+var dataKeyId_1 = (await clientEncryption.GetKeyByAlternateKeyNameAsync("dataKey1"))["_id"];
+if (dataKeyId_1.IsBsonNull)
+{
+    Console.WriteLine("Failed to find DEK");
+    return;
+}
 
 // Do deterministic fields
 var deterministicEncryptOptions = new EncryptOptions(EncryptionAlgorithm.AEAD_AES_256_CBC_HMAC_SHA_512_Deterministic, keyId: dataKeyId_1.AsGuid);
